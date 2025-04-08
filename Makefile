a3f001d3140f74a6ec15baebb7f9ef4f608ec083
Lucca Seabra Cruz <lseabra-@c1r12s4.42porto.com>
2025-04-08 20:53:59 +0100

    add Makefile, ft_isnum, ft_isalpha, ft_isascii, ft_isdigit, ft_isprint and libft.h

---
+++ Makefile
@@ -0,0 +1,30 @@
+NAME = libft.a
+SRCS =  
+	ft_isalpha.c \
+	ft_isdigit.c \
+	ft_isalnum.c \
+	ft_isascii.c \
+	ft_isprint.c
+OBJS = ${SRCS:.c=.o}
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+RM = rm -f
+LIBC = ar rcs
+
+.c.o:
+	${CC} ${CFLAGS} -c $< -o ${<:.c=.o}
+
+all: ${NAME}
+
+${NAME}: ${SRCS} libft.h
+	${LIBC} ${NAME} ${OBJS}
+
+clean:
+	${RM} ${OBJS}
+
+fclean: clean
+	${RM} ${NAME}
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/08 13:41:17 by lseabra-          #+#    #+#             */
+/*   Updated: 2025/04/08 20:48:28 by lseabra-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdio.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+
+#endif
