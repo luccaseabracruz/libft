7be5919a40d2a17a6f23db86cc9f57e5baf47b7b
Lucca Seabra Cruz <lseabra-@c1r7s4.42porto.com>
2025-04-18 15:11:32 +0100

    create doc for ft_toupper

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,23 +6,37 @@
 /*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 15:54:10 by lseabra-          #+#    #+#             */
-/*   Updated: 2025/04/10 16:05:00 by lseabra-         ###   ########.fr       */
+/*   Updated: 2025/04/18 15:11:18 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * ft_toupper - Converts a lowercase alphabetic character to its uppercase 
+ * equivalent. If the character is not a lowercase alphabetic character, it 
+ * is returned unchanged.
+ * @param c The character to be converted.
+ * 
+ * @return: The uppercase equivalent if c is a lowercase letter, otherwise c.
+ */
 int	ft_toupper(int c)
 {
 	if (ft_isalpha(c) && c >= 97)
 		c -= 32;
 	return (c);
 }
-/* 
-int	main(void)
-{
-	int	c = -4;
 
-	printf("c: %c\nnew c: %c", c, ft_toupper(c));
-}
- */
+/* int	main(void)
+{
+	// Test cases for ft_toupper
+	char test_chars[] = {'a', 'z', 'A', 'Z', '1', '!', '\n'};
+	int i;
+
+	for (i = 0; i < 7; i++)
+	{
+		char c = test_chars[i];
+		printf("Original: %c, Uppercase: %c\n", c, ft_toupper(c));
+	}
+	return (0);
+} */
