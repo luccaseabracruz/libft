a5b84dea528b8ec58d35f087ed99d9ed0aba44be
Lucca Seabra Cruz <lseabra-@c1r7s4.42porto.com>
2025-04-18 12:01:47 +0100

    create documentation for ft_lstadd_back

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,12 +6,20 @@
 /*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 20:25:28 by lseabra-          #+#    #+#             */
-/*   Updated: 2025/04/16 22:19:53 by lseabra-         ###   ########.fr       */
+/*   Updated: 2025/04/18 12:01:01 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * ft_lstadd_back - Adds a new node to the end of a linked list.
+ * @param lst: A pointer to the pointer of the first node of the list.
+ * @param new: a pointer to the new node to be added to the list.
+ *
+ * If the list is empty, the new node becomes the first node.
+ * Otherwise, the new node is appended to the last node of the list.
+ */
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
