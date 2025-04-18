1fb30708cd124f39d3fe3292097f57c5f18472de
Lucca Seabra Cruz <lseabra-@c1r7s4.42porto.com>
2025-04-18 13:51:52 +0100

    create doc for ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,12 +6,21 @@
 /*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 19:02:00 by lseabra-          #+#    #+#             */
-/*   Updated: 2025/04/10 19:38:15 by lseabra-         ###   ########.fr       */
+/*   Updated: 2025/04/18 13:51:26 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * ft_memchr - Locates the first occurrence of a character in a memory block.
+ * @param s: Pointer to the memory block to search.
+ * @param c: Character to locate (converted to unsigned char).
+ * @param n: Number of bytes to search.
+ *
+ * @return A pointer to the first occurrence of the character in the memory
+ * block, or NULL if the character is not found within the first n bytes.
+ */
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*ptr;
@@ -26,3 +35,19 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	}
 	return (NULL);
 }
+
+/* #include <stdio.h>
+
+int	main(void)
+{
+	const char	str[] = "Hello, world!";
+	char		ch = 'o';
+	char		*result;
+
+	result = ft_memchr(str, ch, sizeof(str));
+	if (result)
+		printf("Character '%c' found at position: %ld\n", ch, result - str);
+	else
+		printf("Character '%c' not found.\n", ch);
+	return (0);
+} */
