6cf12061532c4fd026cb2e309f07d86f2f7355d8
Lucca Seabra Cruz <lseabra-@c1r7s4.42porto.com>
2025-04-18 14:02:48 +0100

    refactor the doc of ft_memmove and create tests

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,11 +6,13 @@
 /*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 16:07:10 by lseabra-          #+#    #+#             */
-/*   Updated: 2025/04/18 11:33:54 by lseabra-         ###   ########.fr       */
+/*   Updated: 2025/04/18 14:02:19 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
  * This file contains the implementation of the ft_memmove function, which
@@ -19,11 +21,11 @@
  * memory safely when the source and destination overlap.
  * 
  *  * ft_memmove - Copies memory from one location to another, handling overlap.
- * @dst: Pointer to the destination memory block.
- * @src: Pointer to the source memory block.
- * @n: The number of bytes to copy.
+ * @param dst: Pointer to the destination memory block.
+ * @param src: Pointer to the source memory block.
+ * @param n: The number of bytes to copy.
  *
- * Return: A pointer to the destination memory block.
+ * @return A pointer to the destination memory block.
  * This function is safe to use when memory areas overlap.
  */
 static void	*ft_backwards_memcpy(void *dest, const void *src, size_t n)
@@ -55,3 +57,27 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 		ft_memcpy(dest, src, n);
 	return (dest);
 }
+
+/* int main(void)
+{
+	char src1[] = "Hello, World!";
+	char dest1[20];
+	char src2[] = "Overlap Test";
+	char dest2[20];
+
+	// Test 1: Non-overlapping memory
+	ft_memmove(dest1, src1, strlen(src1) + 1);
+	printf("Test 1 - Non-overlapping: %s\n", dest1);
+
+	// Test 2: Overlapping memory (src < dest)
+	strcpy(dest2, src2);
+	ft_memmove(dest2 + 3, dest2, strlen(dest2) + 1);
+	printf("Test 2 - Overlapping (src < dest): %s\n", dest2);
+
+	// Test 3: Overlapping memory (dest < src)
+	strcpy(dest2, src2);
+	ft_memmove(dest2, dest2 + 3, strlen(dest2 + 3) + 1);
+	printf("Test 3 - Overlapping (dest < src): %s\n", dest2);
+
+	return 0;
+} */
