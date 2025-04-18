05c32c8ace3b8925d1c198ad9075e2659bbb245b
Lucca Seabra Cruz <lseabra-@c1r7s4.42porto.com>
2025-04-18 14:52:40 +0100

    create doc for ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,12 +6,20 @@
 /*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 17:18:31 by lseabra-          #+#    #+#             */
-/*   Updated: 2025/04/10 17:47:32 by lseabra-         ###   ########.fr       */
+/*   Updated: 2025/04/18 14:52:14 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * ft_strrchr - Locates the last occurrence of a character in a string.
+ * @param s: The string to be searched.
+ * @param c: The character to locate (converted to a char).
+ *
+ * @return: A pointer to the last occurrence of the character in the string,
+ *         or NULL if the character is not found.
+ */
 char	*ft_strrchr(const char *s, int c)
 {
 	const char	*ptr;
@@ -32,8 +40,8 @@ char	*ft_strrchr(const char *s, int c)
 	else
 		return (NULL);
 }
-/* 
-int	main(void)
+
+/* int	main(void)
 {
 	char	*s = "AaBcCDd";
 	int		c = 92;
@@ -46,6 +54,5 @@ int	main(void)
 			original: %s\n
 			My: %s\n"
 			, strrchr(s, c), ft_strrchr(s, c)
-		);
-}
- */
+			);
+}*/
