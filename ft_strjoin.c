ee008b4bb822d209ad905931e9f6cc691a2c3f87
Lucca Seabra Cruz <lseabra-@c1r7s4.42porto.com>
2025-04-18 14:32:29 +0100

    create doc for ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,12 +6,22 @@
 /*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 16:50:07 by lseabra-          #+#    #+#             */
-/*   Updated: 2025/04/11 18:05:40 by lseabra-         ###   ########.fr       */
+/*   Updated: 2025/04/18 14:32:04 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * ft_strjoin - Allocates and returns a new string, which is the result of
+ * concatenating 's1' and 's2'.
+ *
+ * @param s1: The prefix string.
+ * @param s2: The suffix string.
+ *
+ * @return: A pointer to the newly allocated string containing the concatenation
+ * of 's1' and 's2', or NULL if the allocation fails.
+ */
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*ptr;
@@ -22,14 +32,16 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	s2_len = ft_strlen(s2);
 	ptr = malloc((s1_len + s2_len + 1) * sizeof(char));
 	if (!ptr)
+	{
 		return (NULL);
+	}
 	ft_bzero(ptr, (s1_len + s2_len + 1) * sizeof(char));
 	ft_strlcat(ptr, s1, s1_len + 1);
 	ft_strlcat(ptr, s2, s1_len + s2_len + 1);
 	return (ptr);
 }
-/* 
-int	main(void)
+
+/* int	main(void)
 {
 	char const	*s1 = "my favorite animal is";
 	char const	*s2 = "";
@@ -42,5 +54,4 @@ int	main(void)
 	printf("concatenated: %s\n", res);
 	free(res);
 	free(temp);
-}
- */
+} */
