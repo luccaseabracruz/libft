2574f84d558609f4d4728d5c588868106b503602
Lucca Seabra Cruz <lseabra-@c1r7s4.42porto.com>
2025-04-18 15:16:55 +0100

    refactor ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 13:29:24 by lseabra-          #+#    #+#             */
-/*   Updated: 2025/04/18 11:53:49 by lseabra-         ###   ########.fr       */
+/*   Updated: 2025/04/18 15:16:36 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,14 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*ptr;
 
 	if (nmemb == 0 || size == 0)
+	{
 		return (malloc(0));
+	}
 	ptr = malloc(nmemb * size);
 	if (!ptr)
+	{
 		return (NULL);
+	}
 	ft_bzero(ptr, nmemb * size);
 	return (ptr);
 }
